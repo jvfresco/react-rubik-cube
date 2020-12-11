95f996527f11c9017b5923d919a7ca7ccbf1baec
Fispe <javifge@gmail.com>
2020-12-11 18:23:45 +0100

    Updates

---
+++ precache-manifest.af87e9e71d5cf3d4f439a3fe3c08196c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc2f9132cda036020ce7d70d87450c1",
+    "url": "/react-rubik-cube/index.html"
+  },
+  {
+    "revision": "dc5bb7a9dbc6e69510dd",
+    "url": "/react-rubik-cube/static/css/main.0d0f3731.chunk.css"
+  },
+  {
+    "revision": "295b1ef8be044c4979e7",
+    "url": "/react-rubik-cube/static/js/2.6be984a5.chunk.js"
+  },
+  {
+    "revision": "4e504eae0f9f3a41bcd4aadbecad12c6",
+    "url": "/react-rubik-cube/static/js/2.6be984a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5bb7a9dbc6e69510dd",
+    "url": "/react-rubik-cube/static/js/main.e0c0d534.chunk.js"
+  },
+  {
+    "revision": "5b0c36d2e9c6f3267d5a",
+    "url": "/react-rubik-cube/static/js/runtime-main.6f24f1ba.js"
+  }
+]);
